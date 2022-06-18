539e70c1b590a67f8d19df49c48454a4f488db91
rodrigo <rodrigo_hip27989@hotmail.com>
2022-06-17 22:53:31 -0500

    Se agrego validacion para numeros negativos y se corrigio los mensajes de alerta que no se mostraban

--- src/app/components/validation-reactive-product/validation-reactive-product.component.ts
+++ src/app/components/validation-reactive-product/validation-reactive-product.component.ts
@@ -26,9 +26,9 @@ export class ValidationReactiveProductComponent implements OnInit {
     }
     this.form = this.fb.group({
       nombre: new FormControl(this.product_test_INPUT.nombre, [Validators.required, Validators.maxLength(32), Validators.pattern("^[A-Za-z]+[A-Za-z\ ]*[A-Za-z]+")]),
-      cantidad: new FormControl(this.product_test_INPUT.cantidad, [Validators.required, Validators.max(99999999)]),
+      cantidad: new FormControl(this.product_test_INPUT.cantidad, [Validators.required, Validators.min(1), Validators.max(99999999)]),
       medida: new FormControl(this.product_test_INPUT.medida, [Validators.required, Validators.maxLength(16), Validators.pattern("^[A-Za-z]+[A-Za-z\ ]*[A-Za-z]+")]),
-      precio: new FormControl(this.product_test_INPUT.precio, [Validators.required, Validators.max(99999999)]),
+      precio: new FormControl(this.product_test_INPUT.precio, [Validators.required, Validators.min(0), Validators.max(99999999)]),
     });
   }
 
@@ -51,9 +51,9 @@ export class ValidationReactiveProductComponent implements OnInit {
   generarEjemplo(){
     this.form = this.fb.group({
       nombre: [Randoms.getStr(7), [Validators.required, Validators.maxLength(32), Validators.pattern("^[A-Za-z]+[A-Za-z\ ]*[A-Za-z]+")]],
-      cantidad: [Randoms.getInt(2), [Validators.required, Validators.max(99999999)]],
+      cantidad: [Randoms.getInt(2), [Validators.required, Validators.min(1), Validators.max(99999999)]],
       medida: ["Piezas", [Validators.required, Validators.maxLength(16), Validators.pattern("^[A-Za-z]+[A-Za-z\ ]*[A-Za-z]+")]],
-      precio: [Randoms.getInt(2), [Validators.required, Validators.max(99999999)]],
+      precio: [Randoms.getInt(2), [Validators.required, Validators.min(0), Validators.max(99999999)]],
     });
   }
   
