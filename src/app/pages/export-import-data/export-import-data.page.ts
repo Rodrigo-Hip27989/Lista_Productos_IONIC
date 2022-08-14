4c4ad808383cb1c6e23177f3b7c5d5f3d5fbfb85
rodrigo <rodrigohip27989@disroot.org>
2022-08-14 14:07:01 -0500

    Refactorizacion de la pagina 'export-import-data' - Nombres de funciones principalmente

--- src/app/pages/export-import-data/export-import-data.page.ts
+++ src/app/pages/export-import-data/export-import-data.page.ts
@@ -16,29 +16,32 @@ import { Validations } from 'src/app/funciones/validations';
 
 export class ExportImportDataPage implements OnInit{
   products_array: Producto[];
-  products_token: string;
-  path_directory: string;
-  path_directory_temp: string;
-  name_file: string;
+  file_name: string;
+  file_directory: string;
+  file_directory_share: string;
+  //Centinelas
   private is_mobile_platform: boolean;
-  valid_path_directory;
-  valid_name_file;
+  valid_file_directory;
+  valid_file_name;
+  //Tokens para recuperar del localstorage
+  products_token: string;
+
 
   constructor(private papa: Papa, private plt: Platform) {
     this.products_token = "products_array";
-    this.name_file = "listaproducts";
-    this.path_directory = "Toronja/ListaProductos";
-    this.path_directory_temp = "Download";
+    this.file_name = "lista_products";
+    this.file_directory = "Toronja/Lista_Productos";
+    this.file_directory_share = "Download";
     this.is_mobile_platform = false;
-    this.valid_path_directory = false;
-    this.valid_name_file = false;
+    this.valid_file_directory = false;
+    this.valid_file_name = false;
   }
 
   ngOnInit(): void {
     this.products_array = JSON.parse(localStorage.getItem(this.products_token));
     if (this.plt.is('capacitor')) { // Producción
       this.is_mobile_platform = true;
-      AndroidFiles.create_directory(this.path_directory);
+      AndroidFiles.create_directory(this.file_directory);
     }
     else if(this.plt.is('desktop') || this.plt.is('mobileweb')){ //Desarrollo
       this.is_mobile_platform = false;
@@ -48,69 +51,69 @@ export class ExportImportDataPage implements OnInit{
   // FUNCIONES PRINCIPALES
 
   async export_products_csv(){
-    const exportar_archivo = () => {
-      AndroidFiles.export_file(this.papa.unparse(this.products_array), this.path_directory, `${this.name_file}.csv`);
+    const exportar_csv = () => {
+      AndroidFiles.export_file(this.papa.unparse(this.products_array), this.file_directory, `${this.file_name}.csv`);
       Messages.toast_top("Archivo exportado correctamente!");
     }
-    await this.export_products(this.papa.unparse(this.products_array), this.path_directory, this.name_file, ".csv", exportar_archivo);
+    await this.export_any_file(this.file_directory, `${this.file_name}.csv`, exportar_csv);
   }
 
   async export_products_json(){
-    const exportar_archivo = () => {
-      AndroidFiles.export_file(localStorage.getItem(this.products_token), this.path_directory, `${this.name_file}.json`);
+    const exportar_json = () => {
+      AndroidFiles.export_file(localStorage.getItem(this.products_token), this.file_directory, `${this.file_name}.json`);
       Messages.toast_top("Archivo exportado correctamente!");
     }
-    await this.export_products(localStorage.getItem(this.products_token), this.path_directory, this.name_file, ".json", exportar_archivo);
+    await this.export_any_file(this.file_directory, `${this.file_name}.json`, exportar_json);
   }
 
   async import_products_csv(){
     const importar_csv = async () => {
-      const contents = await AndroidFiles.read_file(this.path_directory, `${this.name_file}.csv`);
+      const contents = await AndroidFiles.read_file(this.file_directory, `${this.file_name}.csv`);
       this.products_array.splice(0);
       this.products_array = this.convert_csv_to_array_products(contents);
       this.update_localstorage();
       await Messages.toast_top("Archivo importado correctamente!");
     }
-    await this.import_products(this.path_directory, this.name_file, ".csv", importar_csv);
+    await this.import_any_file(this.file_directory, `${this.file_name}.csv`, importar_csv);
   }
 
   async import_products_json(){
     const importar_json = async () => {
-      const contents = await AndroidFiles.read_file(this.path_directory, `${this.name_file}.json`);
+      const contents = await AndroidFiles.read_file(this.file_directory, `${this.file_name}.json`);
       this.products_array.splice(0);
       this.products_array = JSON.parse(contents.data);
       this.update_localstorage();
       await Messages.toast_top("Archivo importado correctamente!");
     }
-    await this.import_products(this.path_directory, this.name_file, ".json", importar_json);
+    await this.import_any_file(this.file_directory, `${this.file_name}.json`, importar_json);
   }
 
   async delete_file_products_csv(){
-    this.delete_file_products(this.path_directory, this.name_file, ".csv");
+    this.delete_any_file(this.file_directory, `${this.file_name}.csv`);
   }
 
   async delete_file_products_json(){
-    this.delete_file_products(this.path_directory, this.name_file, ".json");
+    this.delete_any_file(this.file_directory, `${this.file_name}.json`);
   }
 
   async share_file_product_list_csv(){
-    let recovered_data: string = this.papa.unparse(this.products_array);
-    let description_msg: string = `${this.name_file} - ${this.getCurrentDate()}`;
-    await this.share_file_product_list(recovered_data, this.path_directory_temp, `${this.name_file}.csv`, description_msg);
+    let recovered_data_csv: string = this.papa.unparse(this.products_array);
+    let description_msg: string = `${this.file_name} - ${this.getCurrentDate()}`;
+    await this.share_any_file(this.file_directory_share, `${this.file_name}.csv`, description_msg, recovered_data_csv);
   }
 
   async share_file_product_list_json(){
-    let recovered_data: string = localStorage.getItem(this.products_token);
-    let description_msg: string = `${this.name_file} - ${this.getCurrentDate()}`;
-    await this.share_file_product_list(recovered_data, this.path_directory_temp, `${this.name_file}.json`, description_msg);
+    let recovered_data_json: string = localStorage.getItem(this.products_token);
+    let description_msg: string = `${this.file_name} - ${this.getCurrentDate()}`;
+    await this.share_any_file(this.file_directory_share, `${this.file_name}.json`, description_msg, recovered_data_json);
   }
 
   // FUNCIONES PRINCIPALES
 
-  async export_products(datos: string, ruta_carpeta: string, nombre_archivo: string, extension: string, accion_de_exportar: any){
-    AndroidFiles.create_directory(this.path_directory);
-    const content_directory = await AndroidFiles.read_directory(ruta_carpeta);
-    if(await content_directory.files.indexOf(`${nombre_archivo}${extension}`) === -1){
+  async export_any_file(file_directory: string, full_file_name: string, accion_de_exportar: any){
+    AndroidFiles.create_directory(file_directory);
+    const content_directory = await AndroidFiles.read_directory(file_directory);
+    if(await content_directory.files.indexOf(`${full_file_name}`) === -1){
       accion_de_exportar();
     }
     else{
@@ -118,34 +121,34 @@ export class ExportImportDataPage implements OnInit{
     }
   }
 
-  async import_products(ruta_carpeta: string, nombre_archivo: string, extension: string, accion_de_importar: any){
-    AndroidFiles.create_directory(this.path_directory);
-    const content_directory = await AndroidFiles.read_directory(ruta_carpeta);
-    if(await content_directory.files.indexOf(`${nombre_archivo}${extension}`) === -1){
-      await Messages.alert_ok("File not found!", `\n${ruta_carpeta}/${nombre_archivo}${extension}`);
+  async import_any_file(file_directory: string, full_file_name: string, accion_de_importar: any){
+    AndroidFiles.create_directory(file_directory);
+    const content_directory = await AndroidFiles.read_directory(file_directory);
+    if(await content_directory.files.indexOf(`${full_file_name}`) === -1){
+      await Messages.alert_ok("File not found!", `\n${file_directory}/${full_file_name}`);
     }
     else{
       await accion_de_importar();
     }
   }
 
-  async delete_file_products(ruta_carpeta: string, nombre_archivo: string, extension: string){
-    AndroidFiles.create_directory(this.path_directory);
-    const content_directory = await AndroidFiles.read_directory(ruta_carpeta);
-    if(await content_directory.files.indexOf(`${nombre_archivo}${extension}`) === -1){
-      await Messages.alert_ok("File not found!", `\n${ruta_carpeta}/${nombre_archivo}${extension}`);
+  async delete_any_file(file_directory: string, full_file_name: string){
+    AndroidFiles.create_directory(this.file_directory);
+    const content_directory = await AndroidFiles.read_directory(file_directory);
+    if(await content_directory.files.indexOf(`${full_file_name}`) === -1){
+      await Messages.alert_ok("File not found!", `\n${file_directory}/${full_file_name}`);
     }
     else{
-      await AndroidFiles.delete_file(ruta_carpeta, `${nombre_archivo}${extension}`);
+      await AndroidFiles.delete_file(file_directory, `${full_file_name}`);
       Messages.toast_top("Archivo eliminado");
     }
   }
 
-  async share_file_product_list(data_to_string: string, path_file: string, full_file_name: string, description_msg: string){
-    AndroidFiles.create_directory(path_file);
-    await AndroidFiles.export_file(data_to_string, path_file, full_file_name);
-    await AndroidFiles.share_file(path_file, full_file_name, description_msg);
-    await AndroidFiles.delete_file(path_file, full_file_name);
+  async share_any_file(file_directory: string, full_file_name: string, description_msg: string, data_to_string: string){
+    AndroidFiles.create_directory(file_directory);
+    await AndroidFiles.export_file(data_to_string, file_directory, full_file_name);
+    await AndroidFiles.share_file(file_directory, full_file_name, description_msg);
+    await AndroidFiles.delete_file(file_directory, full_file_name);
   }
 
   // FUNCIONES SECUNDARIAS
@@ -154,10 +157,10 @@ export class ExportImportDataPage implements OnInit{
     let posibles_directorios: string[] = ruta_directorio.split('/');
     for(let i: number = 0; i< posibles_directorios.length; i++){
       if(Validations.regex_no_blank_space.test(posibles_directorios[i])){
-        this.valid_path_directory = true;
+        this.valid_file_directory = true;
       }
       else{
-        this.valid_path_directory = false;
+        this.valid_file_directory = false;
         Messages.toast("Solo se permiten letras!", "middle", 2500);
         break;
       }
@@ -166,7 +169,7 @@ export class ExportImportDataPage implements OnInit{
 
   validate_name_file(ruta_archivo: string): void{
     if(Validations.regex_no_blank_space.test(ruta_archivo)){
-      this.valid_name_file = true;
+      this.valid_file_name = true;
     }
     else{
       Messages.toast("Solo se permiten letras!", "middle", 2500);
@@ -201,24 +204,24 @@ export class ExportImportDataPage implements OnInit{
   // FUNCIONES PARA TESTING
 
   public export_products_csv_desktop(){
-    AndroidFiles.export_file_desktop(this.papa.unparse(this.products_array), `${this.name_file} - ${this.getCurrentDate()}`, ".csv");
+    AndroidFiles.export_file_desktop(this.papa.unparse(this.products_array), `${this.file_name} - ${this.getCurrentDate()}`, ".csv");
     Messages.toast_top("Archivo descargado (desktop)");
   }
 
   public export_products_json_desktop(){
-    AndroidFiles.export_file_desktop(localStorage.getItem(this.products_token), `${this.name_file} - ${this.getCurrentDate()}`, ".json");
+    AndroidFiles.export_file_desktop(localStorage.getItem(this.products_token), `${this.file_name} - ${this.getCurrentDate()}`, ".json");
     Messages.toast_top("Archivo descargado (desktop)");
   }
 
   async ver_archivos_y_directorios(){
-    const content_directory = await AndroidFiles.read_directory(this.path_directory);
+    const content_directory = await AndroidFiles.read_directory(this.file_directory);
     let list_of_files: string[] = content_directory.files;
     if(list_of_files.length === 0){
       await Messages.alert_ok("Resultado!", `No se encontraron archivos`);
     }
     else{
       for(let i: number = 0; i<list_of_files.length; i++){
-        const info_file = await AndroidFiles.about_file(`${this.path_directory}/${list_of_files[i]}`);;
+        const info_file = await AndroidFiles.about_file(`${this.file_directory}/${list_of_files[i]}`);
         await Messages.alert_ok(`File [${i}]: ${list_of_files[i]}`, `${info_file.type}\n${info_file.size}\n${info_file.mtime}\n${info_file.uri}`);
       }
     }
