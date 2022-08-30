cd238f4cad4b1fdb3de6c5456366a6068fb0fae4
Rodrigo <Rodrigohip27989@disroot.org>
2022-08-29 19:47:41 -0500

    Se agregarón archivos de configuración de la app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.cap.toronja.mis_gastos',
+  appName: 'Mis Gastos',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
