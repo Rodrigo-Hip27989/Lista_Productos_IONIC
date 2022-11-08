2a218a6ee96cc0d8aa1d79ed94bbc4462f22d8df
Rodrigo <rodrigohip27989@disroot.org>
2022-11-08 00:11:05 -0600

    Se termino el diseño de la pagina 'form-shopping-list'

--- src/app/pages/form-shopping-list/form-shopping-list.module.ts
+++ src/app/pages/form-shopping-list/form-shopping-list.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { FormShoppingListPageRoutingModule } from './form-shopping-list-routing.module';
-
 import { FormShoppingListPage } from './form-shopping-list.page';
+import { DatePickerComponent } from 'src/app/components/date-picker/date-picker.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     FormShoppingListPageRoutingModule
   ],
-  declarations: [FormShoppingListPage]
+  declarations: [FormShoppingListPage, 
+    DatePickerComponent
+  ]
 })
 export class FormShoppingListPageModule {}
--- src/app/pages/form-shopping-list/form-shopping-list.page.ts
+++ src/app/pages/form-shopping-list/form-shopping-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LStorageData } from 'src/app/funciones/local_storage';
+import { ShoppingList } from 'src/app/models/shopping_list';
 
 @Component({
   selector: 'app-form-shopping-list',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-shopping-list.page.scss'],
 })
 export class FormShoppingListPage implements OnInit {
+  this_form: FormGroup;
+  shopping_list_preview !: ShoppingList;
 
-  constructor() { }
+  constructor(private readonly fb: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.shopping_list_preview = new ShoppingList();
+    this.this_form = this.initFormShoppingList()
+  }
+
+  onSubmit(): void {
+    if (this.this_form.invalid) {
+      return;
+    }
+    let shopping_list_submit: ShoppingList = new ShoppingList();
+    for (const propiedad in this.this_form.value) {
+      shopping_list_submit[propiedad] = this.this_form.value[propiedad];
+    }
+    shopping_list_submit.date = this.shopping_list_preview.date;    
+    console.log('Form ->', shopping_list_submit);
+  }
+
+  initFormShoppingList(): FormGroup {
+    //No remover el campo 'date' porque es requerido en la función 'onSubmit' al copiar los valores
+    return this.fb.group({
+      name: ['', [Validators.required, Validators.maxLength(64), Validators.pattern("^[A-Za-z]+[A-Za-z\ ]*[A-Za-z]+")]],
+      products: [ LStorageData.getProductsArray(), Validators.required ],
+      note: ['', [Validators.maxLength(256)]],
+    });
+  }
+
+  save_date_picker(fechaRecivida: string){
+    this.shopping_list_preview.date = fechaRecivida;
   }
 
 }
