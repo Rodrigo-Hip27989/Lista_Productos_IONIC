9beb424eed594bc30144bf3db8e3060721979c09
Rodrigo <rodrigohip27989@disroot.org>
2022-11-08 19:00:49 -0600

    Se creo el componente 'form-shopping-list' para reemplazar a la pagina con el mismo nombre

---
+++ src/app/components/form-shopping-list/form-shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-shopping-list',
+  templateUrl: './form-shopping-list.component.html',
+  styleUrls: ['./form-shopping-list.component.scss'],
+})
+export class FormShoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
